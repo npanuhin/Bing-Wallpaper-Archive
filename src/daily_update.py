0417ae94202a79efe3e01fd5495e3c36cbc0390f
Nikita Panyuhin <n.panuhin@mail.ru>
2022-02-07 09:31:21 +0300

    Added more copyright notices

--- src/daily_update.py
+++ src/daily_update.py
@@ -1,3 +1,10 @@
+#                                    ┌───────────────────────────────────────────┐
+#                                    │    Copyright (c) 2022 Nikita Paniukhin    │
+#                                    │      Licensed under the MIT license       │
+#                                    └───────────────────────────────────────────┘
+#
+# ======================================================================================================================
+
 from peapix import updateRegions, FLAG_LATEST, FLAG_NOTOUCH
 from json import load as json_load, dump as json_dump
 from utils import mkpath
