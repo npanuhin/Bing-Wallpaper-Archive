2a27a449ec6c56ed365158d4e216bbfc8c3c5749
Nikita Panyuhin <n.panuhin@mail.ru>
2022-03-03 01:36:17 +0300

    Fixed copyright field (part 2)

--- src/tools/bingwallpaper.anerg.com/renew_copyright.py
+++ src/tools/bingwallpaper.anerg.com/renew_copyright.py
@@ -39,19 +39,18 @@ api = safe_json.load(mkpath(API_PATH, REGION.upper(), REGION.lower() + ".json"))
 with open("page.html", 'r', encoding="utf-8") as file:
     soup = BeautifulSoup(file.read(), "lxml")
 
-for cur_date in month_year_iter(date(2022, 3, 1), date(2022, 4, 1)):
-    cur_date = date(*cur_date, 1)
-    print("Parsing {}...".format(cur_date.strftime("%Y%m")))
-    soup = BeautifulSoup(req_get("https://bingwallpaper.anerg.com/us/" + cur_date.strftime("%Y%m")).text, "lxml")
+for cur_month in month_year_iter(date(2020, 1, 1), date(2022, 4, 1)):
+    cur_month = date(*cur_month, 1)
+    cur_month_str = cur_month.strftime("%Y-%m")
 
+    print("Parsing {}...".format(cur_month.strftime("%Y%m")))
+    soup = BeautifulSoup(req_get("https://bingwallpaper.anerg.com/us/" + cur_month.strftime("%Y%m")).text, "lxml")
     soup = soup.find("body").find("div", id="jssor_1")
 
-    with open("subpage.html", 'w', encoding="utf-8") as file:
-        file.write(soup.prettify())
+    # with open("subpage.html", 'w', encoding="utf-8") as file:
+    #     file.write(soup.prettify())
 
-    images = soup.find_all("img", {"data-u": "image"})
-
-    for image in images:
+    for image in soup.find_all("img", {"data-u": "image"}):
         title = image.parent.parent.find("div", class_="intro").text
         # image_url = image.get("src")
         # print("https:" + image_url)
@@ -61,25 +60,39 @@ for cur_date in month_year_iter(date(2022, 3, 1), date(2022, 4, 1)):
         if title.endswith("(Bing United States)"):
             title = title[:-len("(Bing United States)")]
 
+        # if '(' in title and ')' in title:
+
         copyright = title[title.rfind('(') + 1:title.rfind(')')]
 
-        assert len(copyright) > 0, "Can not find copyright in {}".format(title)
+        assert len(copyright) > 0 and len(copyright) != len(title), "Can not find copyright in {}".format(title)
 
         title = title[:title.rfind('(')].strip()
 
-        # print(copyright)
-        # print(title)
+        print((title, copyright))
 
         print("Searching {}...".format(title))
-        seach_length, seach_count, search_result = 0, len(api), None
-        while seach_count > 1:
-            seach_length += 1
+        search_result = None
+        for seach_length in range(1, len(title) + 1):
             seach_count = 0
 
             for index, item in enumerate(api):
-                if item["title"][:seach_length] == title[:seach_length]:
+                if item["date"].startswith(cur_month_str) and item["title"].startswith(title[:seach_length]):
                     seach_count += 1
                     search_result = index
+
+                    print(item["title"], title)
+                    print(len(item["title"]), len(title))
+
+                    if len(item["title"]) == len(title):
+                        seach_count = 1
+                        break
+
+            if seach_count <= 1:
+                break
+
+            print()
+
+        print(seach_count, seach_length)
 
         assert seach_count == 1, "Title not found: {}".format(title)
 
@@ -89,6 +102,6 @@ for cur_date in month_year_iter(date(2022, 3, 1), date(2022, 4, 1)):
 
         # print(search_result, api[search_result]["copyright"], copyright)
 
-    break
+    # break
 
 safe_json.dump(mkpath(API_PATH, REGION.upper(), REGION.lower() + ".new.json"), api, prettify=True)
