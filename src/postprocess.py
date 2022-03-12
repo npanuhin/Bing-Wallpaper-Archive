ee7d97feba6be3c44264bf59233f6c9a8c891633
Nikita Panyuhin <n.panuhin@mail.ru>
2022-03-12 23:37:14 +0300

    Fixed postprocess

--- src/postprocess.py
+++ src/postprocess.py
@@ -2,17 +2,26 @@ from json import load as json_load, dump as json_dump
 
 from utils import mkpath
 
+FIELDS = ("title", "caption", "subtitle", "copyright", "description", "date", "path")
 
-def postproces_item(api_data):
-    if api_data["caption"] == api_data["subtitle"]:
-        api_data["caption"] = None
 
-    return api_data
+def postproces_item(image_api):
+    image_api = {
+        key: (image_api[key] if key in image_api else None)
+        for key in FIELDS
+    }
+
+    if image_api["caption"] == image_api["subtitle"]:
+        image_api["caption"] = None
+
+    return image_api
 
 
 def postprocess_api(api):
     for i, item in enumerate(api):
         api[i] = postproces_item(item)
+
+    api.sort(key=lambda image_api: image_api["date"])
 
     return api
 
