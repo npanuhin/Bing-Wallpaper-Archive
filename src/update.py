43dd0e7bf47be5601400432b286c7547639e3976
Nikita Panyuhin <n.panuhin@mail.ru>
2023-12-14 21:09:09 +0100

    Rewrote badges and update.py

--- src/update.py
+++ src/update.py
@@ -1,65 +1,67 @@
-from json import load as json_load, dump as json_dump
-from urllib.parse import urlencode
 from datetime import datetime
-from bing import update_all
-import os
+import json
 import re
 
-from utils import mkpath
+from bing import update_all, REGIONS
+from Region import Region
 
 
-# REGIONS = ["AU", "CA", "CN", "DE", "FR", "IN", "JP", "ES", "GB", "US"]
-# en-WW
-REGIONS = ["en-US"]
-
-API_PATH = "../api"
-
-website_start_date = "2017-05-10"
+WEBSITE_START_DATE = '2017-05-10'
 
 
-# =============================================== Update api and images ================================================
+# ------------------------------------------------ Update api and images -----------------------------------------------
 
 update_all()
 
-latest_image_date = os.path.splitext(max(os.listdir(mkpath(API_PATH, "US", "images"))))[0]
-print("Updated US to", latest_image_date)
+print('Update finished\n')
 
-# =================================================== Update README ====================================================
+# ----------------------------------------------- Fetch last image in US -----------------------------------------------
 
-with open(mkpath("../README.md"), 'r', encoding="utf-8") as file:
+us_api = Region('en-US').read_api()
+
+latest_image = max(us_api, key=lambda item: item['date'])
+
+print(f'Last US image: {latest_image["date"]}')
+
+# ---------------------------------------------------- Update README ---------------------------------------------------
+
+with open('../README.md', 'r', encoding='utf-8') as file:
     readme = file.read()
 
-shields_io_badge = "https://img.shields.io/static/v1?" + urlencode({
-    "label": "Last image",
-    "message": latest_image_date,
-    "color": "informational",
-    "style": "flat"
-})
 readme = re.sub(
-    r"<img alt=\"Last image:.*?\" src=\".+?\">",
-    f'<img alt="Last image: {latest_image_date}" src="{shields_io_badge}">',
+    r'<a id="last_image_link" href=".*?">',
+    f'<a id="last_image_link" href="{latest_image["url"]}">',
     readme
 )
+
 readme = re.sub(
-    r"(^<!-- !Today's image -->$)\n.+\n",
-    fr"\1\n[![](api/US/images/{latest_image_date}.jpg)](https://npanuhin.me/tools/show_bing_image?url=https://raw.githubusercontent.com/npanuhin/bing-wallpaper-archive/master/api/US/images/{latest_image_date}.jpg)\n",
-    readme, flags=re.MULTILINE
+    r'<img id="last_image" title=".*?" alt=".*?" src=".*?">',
+    f'<img id="last_image" title="{latest_image["title"]}" alt="{latest_image["title"]}" src="{latest_image["url"]}">',
+    readme
 )
 
-with open(mkpath("../README.md"), 'w', encoding="utf-8") as file:
+readme = re.sub(
+    r'<img id="last_image_badge" alt=".*?" src=".*?">',
+    f'<img id="last_image_badge" alt="Last image: {latest_image["date"]}" src='
+    f'"https://img.shields.io/badge/Last_image-{latest_image["date"].replace("-", "--")}-informational?style=flat">',
+    readme
+)
+
+with open('../README.md', 'w', encoding='utf-8') as file:
     file.write(readme)
 
-# ================================================= Update website api =================================================
+# ------------------------------------------------- Update website api -------------------------------------------------
 
 for region in REGIONS:
-    country = region.split('-')[1]
-    with open(mkpath("../api", country, country.lower() + ".json"), 'r', encoding="utf-8") as file:
-        api = json_load(file)
+    api = region.read_api()
 
     api_for_website = {
-        datetime.strptime(item["date"], '%Y-%m-%d').strftime('%Y%m%d'): item['title']
-        for item in api if item["date"] >= website_start_date
+        datetime.strptime(item['date'], '%Y-%m-%d').strftime('%Y%m%d'): item['title']
+        for item in api
+        if item['date'] >= WEBSITE_START_DATE
     }
 
-    with open(mkpath("website/api", country.lower() + ".json"), 'w', encoding="utf-8") as file:
-        json_dump(api_for_website, file, ensure_ascii=False, separators=(',', ':'))
+    with open(f'website/api/{region.country}.json', 'w', encoding='utf-8') as file:
+        json.dump(api_for_website, file, ensure_ascii=False, separators=(',', ':'))
+
+    print(f'Updated website api for {region.country.upper()}')
